6fd24df369d5695e0fd6d6b6c22a62986d7a3b55
Wangbj <wangbj@lierda.com>
2020-06-10 14:24:46 +0800

    调用pywinauto模拟输入

--- aiweb_test_tool_V0.2/MyFiles.py
+++ aiweb_test_tool_V0.2/MyFiles.py
@@ -29,13 +29,12 @@ def node_laoding():
     log_list('---------------------------------------')
     task_info_show('载入节点列表')
     log_list('待测节点：')
-    log_list('%s' % (shortID))
+    log_list('%s' % shortID)
     log_list('心跳节点：')
-    log_list('%s' % (shortID_hertBeat))
+    log_list('%s' % shortID_hertBeat)
     log_list('---------------------------------------')
-    # 生成result_dict,用于存储统计结果，并设默认值为0.
     for key in shortID:
-        result_dict.setdefalut(key, 0)
+        result_dict.setdefault(key, 0)
     for key in shortID_hertBeat:
         result_dict.setdefault(key, 0)
     G_Para.set_value('result_dict', result_dict)
